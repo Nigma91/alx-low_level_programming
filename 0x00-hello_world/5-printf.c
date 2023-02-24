973c6c44f64169641aea39aa4f184e197dff7b2d
Fikile Mahlangu <fgmahlangu@gmail.com>
2023-02-23 18:18:25 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints a string using printf
+ * Return: set return to 0
+*/
+
+int main(void)
+{
+	char string1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", string1);
+	return (0);
+}
